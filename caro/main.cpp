63e6ae61797b167be19a3b7176ccb22cada0f4e3
algori <pn2k7hanh@gmail.com>
2023-08-17 17:36:46 +0700

    Update main.cpp

--- caro/main.cpp
+++ caro/main.cpp
@@ -14,7 +14,32 @@ int main(int argc, char** argv)
 {
 	RenderWindow window(VideoMode(800,600),"Caro");
 
-	
+	HMODULE bModule = GetModuleHandle(nullptr);
+	HRSRC bResource = FindResource(bModule, MAKEINTRESOURCE(IDB_BITMAP1), RT_BITMAP);
+
+	HGLOBAL bLoadedResource = LoadResource(nullptr, bResource);
+
+	LPVOID bResourceData = LockResource(bLoadedResource);
+
+	BITMAPINFOHEADER* bBitmapInfoHeader = reinterpret_cast<BITMAPINFOHEADER*>(bResourceData);
+
+	int width = bBitmapInfoHeader->biWidth;
+	int height = bBitmapInfoHeader->biHeight;
+	int bpp = bBitmapInfoHeader->biBitCount;
+
+	int size = width * height * (bpp / 8);
+
+	BYTE* bPixelData = reinterpret_cast<BYTE*>(bResourceData) + sizeof(BITMAPINFOHEADER);
+
+	Texture board;
+	board.loadFromMemory(bPixelData,size);
+
+	Sprite sprite(board);
+
+	cout << width << endl;
+	cout << height << endl;
+	cout << bpp / 8 << endl;
+	cout << size << endl;
 
 
 
@@ -34,7 +59,7 @@ int main(int argc, char** argv)
 
 		window.clear(Color::White);
 
-
+		window.draw(sprite);
 
 		window.display();
 	}
