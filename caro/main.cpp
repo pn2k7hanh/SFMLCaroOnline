3df68e804625777206db59958b7a6a2a0593e381
algori <pn2k7hanh@gmail.com>
2023-08-20 11:56:54 +0700

    add selected box texture

--- caro/main.cpp
+++ caro/main.cpp
@@ -7,25 +7,32 @@
 #include <Windows.h>
 #include <iostream>
 
+
+
 using namespace std;
 using namespace sf;
 
 
 int main(int argc, char** argv)
 {
+	
+
 	const int mtop = 20;
-	const int mleft = 20;
-	const int mright = 20;
-	const int mbottom = 20;
+	const int mleft = 30;
+	const int mright = 40;
+	const int mbottom = 50;
 	
 	int m, n;
-	cin >> m >> n;
+	//cin >> m >> n;
+	m = 20;
+	n = 20;
 
 	int** matrix;
 	matrix = new int* [m];
 	for_each(matrix, matrix + m, [=](int* array) {array = new int[n]; });
 
 	RenderWindow window(VideoMode(mtop+mbottom+ul.w+dr.w+bx.w*n,mleft+mright+ul.h+dr.h+bx.h*m),"Caro");
+
 
 	InitImage();
 	InitTexture();
@@ -47,8 +54,6 @@ int main(int argc, char** argv)
 			tbo(*GetTexture(Tex::BoxO)),
 			tbos(*GetTexture(Tex::BoxOs));
 
-	// Preload Board
-
 	
 
 	while (window.isOpen())
@@ -63,13 +68,52 @@ int main(int argc, char** argv)
 		}
 
 		window.clear(Color::Black);
+		
+		Vector2i pos = Mouse::getPosition(window);
+		cout << pos.x << " " << pos.y << endl;
+
+		// Preload Board
+		tul.setPosition(0, 0); tul.move(mleft, mtop);
+		tur.setPosition((float)ul.w + n * bx.w, 0); tur.move(mleft, mtop);
+		tdl.setPosition(0, (float)ul.h + m * bx.h); tdl.move(mleft, mtop);
+		tdr.setPosition((float)ul.w + n * bx.w, (float)ul.h + m * bx.h); tdr.move(mleft, mtop);
+		window.draw(tul);
+		window.draw(tur);
+		window.draw(tdl);
+		window.draw(tdr);
+		for (int i = 0; i < m; i++)
+		{
+			tleft.setPosition(0, (float)ul.h + i * l.h);
+			tleft.move(mleft, mtop);
+			tright.setPosition((float)ul.w + n * bx.w, (float)ul.h + i * l.h);
+			tright.move(mleft, mtop);
+			window.draw(tleft);
+			window.draw(tright);
+		}
+		for (int i = 0; i < n; i++)
+		{
+			tup.setPosition((float)ul.w + i * u.w, 0);
+			tup.move(mleft, mtop);
+			tdown.setPosition((float)ul.w + i * u.w, (float)ul.h + m * bx.h);
+			tdown.move(mleft, mtop);
+			window.draw(tup);
+			window.draw(tdown);
+		}
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				tb.setPosition((float)i * bx.w, (float)j * bx.h);
+				tb.move((float)mleft + ul.w, (float)mtop + ul.h);
+				window.draw(tb);
+			}
+		}
 
 
 
-
-
-
-
+		//tbx.setPosition(50., 50.);
+		//window.draw(tbx);
+		
 
 
 		window.display();
--- caro/resource.h
+++ caro/resource.h
@@ -7,12 +7,13 @@
 #define IDB_BITMAP3                     111
 #define IDB_BITMAP4                     112
 #define IDB_BITMAP5                     113
+#define IDB_BITMAP6                     115
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        115
+#define _APS_NEXT_RESOURCE_VALUE        116
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
